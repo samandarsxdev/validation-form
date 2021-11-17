585ae3d84bd771be066e4f51d525e79b056af9b6
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-11-17 05:22:09 -0500

    add
    js

---
+++ main.js
@@ -0,0 +1,43 @@
+document.querySelector('#btn').addEventListener('click',function(){myText();}
+
+)
+
+function myText(){
+    let message,text;
+    let errElement = getElementById('#text');
+    message=document.getElementById('#err');
+    text = document.getElementById("text").value;
+   try{
+     if(text==""){
+         errElement.classList.add("err");
+         throw 'bosh';
+     }
+     if(text.lengt>0){
+         errElement.classList.remove("err");
+         errElement.classList.add("valid");
+         message.innerHTML="";
+     }
+    }catch(e){
+        message.innerHTML="qiymat"+e;
+    }
+}
+
+function myPass(){
+    let message,pass;
+    let errElement = getElementById('#password');
+    message=document.getElementById('#err2');
+    pass = document.getElementById("password").value;
+   try{
+     if(pass==""){
+         errElement.classList.add("err2");
+         throw 'bosh';
+     }
+     if(pass.lengt>0){
+         errElement.classList.remove("err2");
+         errElement.classList.add("valid");
+         message.innerHTML="";
+     }
+    }catch(e){
+        message.innerHTML="qiymat"+e;
+    }
+}
